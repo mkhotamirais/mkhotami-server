d82d42bb506ec34cc7f4439919c3a3759213f450
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-14 23:47:02 +0700

    lagi5"

--- app.js
+++ app.js
@@ -18,12 +18,13 @@ app.get("/", (req, res) => {
 
 // app.use("/api/user", require("./routes/userRoute"));
 app.use("/api/user", async (req, res) => {
-  try {
-    const data = await User.find({});
-    ok(res, 200, `getUsers`, data);
-  } catch (error) {
-    err(res, 400, error);
-  }
+  res.json({ name: "ahmad" });
+  // try {
+  //   const data = await User.find({});
+  //   ok(res, 200, `getUsers`, data);
+  // } catch (error) {
+  //   err(res, 400, error);
+  // }
 });
 
 db.then(() => {
