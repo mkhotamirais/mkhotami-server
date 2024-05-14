0f0f72efb0782482f1c69d4fd912d9238be9a8b3
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-15 00:33:19 +0700

    semua

--- config/cred.js
+++ config/cred.js
@@ -12,10 +12,10 @@ const allowedOrigins = [
 ];
 
 const credentials = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   }
   next();
 };
