e9e6052a00a349475564484b2ee351fbf5bf038c
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-15 00:22:57 +0700

    satu

--- config/cred.js
+++ config/cred.js
@@ -13,7 +13,10 @@ const allowedOrigins = [
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) res.header("Access-Control-Allow-Credentials", true);
+  if (allowedOrigins.includes(origin)) {
+    // res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", true);
+  }
   next();
 };
 
