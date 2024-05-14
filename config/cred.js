9efe4fa8aa27aff3d3080fcb9f1149e62bc71238
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-15 00:38:47 +0700

    coba lagin

--- config/cred.js
+++ config/cred.js
@@ -6,25 +6,17 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
-  "http://localhost:5173/",
-  "http://127.0.0.1:5173/",
+  "http://localhost:5173",
   "http://localhost:5174",
 ];
 
 const credentials = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
   }
   next();
 };
-
-// const credentials = (req, res, next) => {
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) res.header("Access-Control-Allow-Credentials", true);
-//   next();
-// };
 
 const corsOptions = {
   origin: function (origin, callback) {
