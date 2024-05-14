9f34b21e82d8009f591ff0fb04caeceb7f93e9d5
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-14 23:34:41 +0700

    coba lagi

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { port } = require("./config/constants");
-const db = require("./config");
-const { credentials, corsOptions } = require("./config/cred");
+// const db = require("./config");
+// const { credentials, corsOptions } = require("./config/cred");
 const app = express();
 const cors = require("cors");
 
-app.use(credentials);
-app.use(cors(corsOptions));
+// app.use(credentials);
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,10 +15,11 @@ app.get("/", (req, res) => {
   res.send(`welcome to mkhotami app`);
 });
 
-app.use("/api/user", require("./routes/userRoute"));
+// app.use("/api/user", require("./routes/userRoute"));
 
-db.then(() => {
-  app.listen(port, () => console.log(`connect to mongodb and listening on port ${port}`));
-}).catch((err) => {
-  console.error(err.message);
-});
+app.listen(port, () => console.log(`connect to mongodb and listening on port ${port}`));
+// db.then(() => {
+//   app.listen(port, () => console.log(`connect to mongodb and listening on port ${port}`));
+// }).catch((err) => {
+//   console.error(err.message);
+// });
