d1b980078cea7b2f71d55137a631c7c942771b33
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-17 08:20:50 +0700

    lagi

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -7,15 +7,15 @@ const {
 } = require("../controllers/productController");
 const { verifyToken, verifyAdmin } = require("../helper/middleware");
 const multer = require("multer");
-// const { join } = require("path");
+const path = require("path");
 
-// const upload = multer({ dest: join(root, "public", "images") });
+const upload = multer({ dest: path.join(__dirname, "..", "public", "images") });
 
 // const { upload } = require("../helper/utils");
 
 const router = require("express").Router();
 
-router.route("/").get(getProducts).post(verifyToken, verifyAdmin, postProduct);
+router.route("/").get(getProducts).post(verifyToken, verifyAdmin, upload.single("image"), postProduct);
 router
   .route("/:id")
   .get(getProductById)
