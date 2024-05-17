102714dd6579df055c8fb288fdc9d5324b9490ed
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-17 08:34:27 +0700

    multer

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -11,15 +11,13 @@ const path = require("path");
 
 const upload = multer({ dest: path.join(__dirname, "..", "public", "images") });
 
-// const { upload } = require("../helper/utils");
-
 const router = require("express").Router();
 
 router.route("/").get(getProducts).post(verifyToken, verifyAdmin, upload.single("image"), postProduct);
 router
   .route("/:id")
   .get(getProductById)
-  .patch(verifyToken, verifyAdmin, updateProduct)
+  .patch(verifyToken, verifyAdmin, upload.single("image"), updateProduct)
   .delete(verifyToken, verifyAdmin, deleteProduct);
 
 module.exports = router;
